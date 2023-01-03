007d280e7a81c7e786c9a13d1944e38197c7f372
Sandrafongshurui <sandrafong90@hotmail.com>
2023-01-03 11:45:47 +0800

    fix: appsepc

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-yarn install
+# Install node.js and PM2 globally
+sudo apt-get update
+sudo apt-get install nodejs-legacy -y
+sudo apt-get install npm  -y
+sudo npm install pm2 -g
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-yarn start
+# Stop all servers and start the server
+pm2 stop all
+pm2 start /home/ubuntu/my-app1/app/app.js
